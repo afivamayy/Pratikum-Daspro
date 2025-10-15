b386bd865e74e12a9d17927abcd6fc80481a31f0
afivamay <afivamay14@gmail.com>
2025-10-15 20:27:56 +0700

    pratikum 1

---
+++ Pratikum 1/Latihan03.java
@@ -0,0 +1,8 @@
+public class Latihan03 {
+    public static void main(String [] args) {
+        System.out.println("Nama saya adalah Linda");
+
+        System.out.println("\nBelajar java itu mudah");
+        // System.out.println("\nSemakin sering mencoba, semakin jago");
+    }
+}
---
+++ Pratikum 1/tugas.java
@@ -0,0 +1,15 @@
+public class tugas {
+    public static void main(String[] args) {
+        System.out.printf("%-20s %-10s %-10s%n", "NIM", "Nama", "Prodi");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020069", "Linda", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020176", "Samsul", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020003", "Yahya", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020189", "Haikal", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020192", "Zona", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020183", "Zaky", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020194", "Irsyad", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020099", "Galang", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020065", "Mayla", "Teknik Informatika");
+        System.out.printf("%-20s %-10s %-10s%n", "245107020005", "Farhan", "Teknik Informatika");
+    }
+}
