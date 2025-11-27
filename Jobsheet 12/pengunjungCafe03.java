72b23e6103c4d8cd2cd79fe8b4f483e4388eb2de
afivamay <afivamay14@gmail.com>
2025-11-27 15:56:02 +0700

    Percobaan 4

---
+++ Jobsheet 12/pengunjungCafe03.java
@@ -0,0 +1,14 @@
+public class pengunjungCafe03 {
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
